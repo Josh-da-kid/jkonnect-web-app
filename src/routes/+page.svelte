baefe839023b08aef2d95c36bb293fbd51cf121b
Josiah <carmenjosh84@gmail.com>
2025-02-22 00:17:17 +0100

    changes made

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -139,7 +139,11 @@
  </section>
 
  <!-- ABOUT ME -->
+<<<<<<< HEAD
  <section id="about" class="p-2">
+=======
+ <section id="about" class="p-6">
+>>>>>>> centered
     <div class="text-4xl sm:text-5xl md:text-6xl text-center mt-21 items-center justify-center">
 
         <h1>About me</h1>
